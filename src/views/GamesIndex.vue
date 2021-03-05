8540325e3f22b7a2a1229c6a160a17139f22260a
Emanuel Sanchez <emmanuels8001@gmail.com>
2021-03-04 21:07:28 -0600

    GameIndex route working

---
+++ src/views/GamesIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="games-index">
+    <h1>All Games</h1>
+    <div v-for="game in games" v-bind:key="game.id">
+      <h1>{{ game.name }}</h1>
+      <h2>{{ game.price }}</h2>
+      <img v-bind:src="game.cover_art" v-bind:alt="game.name" contain height="200px" width="300px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      games: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/games").then(response => {
+      console.log("games index", response);
+      this.games = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
